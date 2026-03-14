0187db589e28c00a951941b44a1abc602fde55b2
Lana <svetlana.mur.85@gmail.com>
2026-03-14 15:50:29 +0000

    Add Task form, index view, basic Bootstrap styling and icons for buttons

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -17,13 +17,16 @@ final class TaskController extends AbstractController
     #[Route('/', name: 'home', methods: ['GET'])]
     #[Route('/task', name: 'app_task_index', methods: ['GET'])]
     public function index(TaskRepository $taskRepository): Response
-    {
+    {   
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
         return $this->render('task/index.html.twig', [
             'tasks' => $taskRepository->findAll(),
+            'form' => $form->createView(),
         ]);
     }
 
-    #[Route('/new', name: 'app_task_new', methods: ['GET', 'POST'])]
+    #[Route('/task/new', name: 'app_task_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $task = new Task();
@@ -34,12 +37,12 @@ final class TaskController extends AbstractController
             $entityManager->persist($task);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_task_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_task_index');
         }
 
         return $this->render('task/new.html.twig', [
             'task' => $task,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -6,11 +6,36 @@ use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title',
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'required' => false,
+                'attr' => ['class' => 'form-control mb-3', 'rows' => 3]
+            ])
+            ->add('status', ChoiceType::class, [
+                'label' => 'Status',
+                'choices' => Task::getStatuses(),
+                'attr' => ['class' => 'form-select mb-3']
+            ])
+            ->add('dueDate', DateTimeType::class, [
+                'label' => 'Due Date',
+                'widget' => 'single_text',
+                'required' => false,
+                'attr' => ['class' => 'form-control mb-3']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
