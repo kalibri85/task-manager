7739ad01727f377d90c172aa4a6bf74dbe8d6735
Lana <svetlana.mur.85@gmail.com>
2026-03-14 01:48:26 +0000

    Current Task entity with fields added, ready for form improvements

---
+++ migrations/Version20260314012350.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260314012350 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task ADD title VARCHAR(255) NOT NULL, ADD description LONGTEXT DEFAULT NULL, ADD status VARCHAR(100) NOT NULL, ADD due_date DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE task DROP title, DROP description, DROP status, DROP due_date');
+    }
+}
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
@@ -13,8 +14,81 @@ class Task
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $status = null;
+
+   #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $dueDate = null;
+
+    public const STATUS_PENDING = 'pending';
+    public const STATUS_IN_PROGRESS = 'in_progress';
+    public const STATUS_DONE = 'done';
+    
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+        public static function getStatuses(): array
+    {
+        return[
+            'Pending' => self::STATUS_PENDING,
+            'In Progress' => self::STATUS_IN_PROGRESS,
+            'Done' => self::STATUS_DONE,
+        ];
+    }
+    
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTime
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTime $dueDate): static
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
     }
 }
